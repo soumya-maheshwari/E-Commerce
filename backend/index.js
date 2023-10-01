5e864ac564cbef31e9754b9cb7c246eb667b3bfd
Soumya Maheshwari <soumyamaheshwari011@gmail.com>
2023-10-01 14:09:54 +0530

    API ENDPOINTS for authentication

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+//
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+const authRoutes = require("./routes/authRoutes");
+
+const { errorMiddleware } = require("./middlewares/ErrorHandler");
+app.use(express.json());
+app.use(cors({ origin: true }));
+app.use(express.urlencoded({ extended: false }));
+const server = app.listen(process.env.PORT);
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("database connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+connectDB();
+console.log(`Connected to port ${process.env.PORT}`);
+
+// Global Error Handling
+app.use(errorMiddleware);
+
+//Routes
+app.use(authRoutes, errorMiddleware);
